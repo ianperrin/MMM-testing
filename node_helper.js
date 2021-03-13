2c319ceb25f3956979ea22f80531b3f79147b49e
Ian <ianperrin@gmail.com>
2021-03-13 23:04:49 +0000

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -1,15 +1,13 @@
 const NodeHelper = require("node_helper");
 
 module.exports = NodeHelper.create({
-	// Override start method.
-	start: function () {
-	},
+  // Override start method.
+  start: function () {},
 
-	// Override socketNotificationReceived received.
-	socketNotificationReceived: function (notification, payload) {
-	},
-  
-	callApi: function () {
-		return 1;
-	}
+  // Override socketNotificationReceived received.
+  socketNotificationReceived: function (notification, payload) {},
+
+  callApi: function () {
+    return 1;
+  }
 });
