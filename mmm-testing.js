57b4912d03a15443f093e4c0e286a3103c0f4e74
Ian <ianperrin@gmail.com>
2021-03-13 07:01:26 +0000

    Create mmm-testing.js

---
+++ mmm-testing.js
@@ -0,0 +1,20 @@
+/* Magic Mirror
+ * Module: MMM-Testing
+ *
+ * By Michael Teeuw https://michaelteeuw.nl
+ * MIT Licensed.
+ */
+Module.register("mmm-testing", {
+	// Default module config.
+	defaults: {
+		text: "Hello World!"
+	},
+
+	getTemplate: function () {
+		return "mmm-testing.njk";
+	},
+
+	getTemplateData: function () {
+		return this.config;
+	}
+});
