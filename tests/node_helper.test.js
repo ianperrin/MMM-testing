e88d7fa2103fc584df9dcd0532dc92978d7fc7db
Ian <ianperrin@gmail.com>
2021-03-13 15:58:29 +0000

    Update node_helper.test.js

--- tests/node_helper.test.js
+++ tests/node_helper.test.js
@@ -1,13 +1,15 @@
 const expect = require("chai").expect;
+const moduleAlias = require('module-alias');
 
 describe("Functions into node_helper.js", function () {
 	before(function () {
 		console.log("Before function");
+		moduleAlias.addAlias('node_helper', "../../../js/node_helper.js")
 		require("../node_helper.js");
 	});
 
 	describe("callApi", function () {
-		console.log("Here");
+		console.log("callApi tests...");
 		it(`for 1 should be return 1}`, function () {
 			expect(1).to.equal(1);
 		});
