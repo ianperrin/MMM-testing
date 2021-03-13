fd4b72bcfb1afe5afdca5d96df140095ca1b1bbb
Ian <ianperrin@gmail.com>
2021-03-13 12:04:44 +0000

    Update mmm-testing.test.js

--- tests/mmm-testing.test.js
+++ tests/mmm-testing.test.js
@@ -1,1 +1,16 @@
+const expect = require("chai").expect;
 
+describe("Functions into mmm-testing.js", function () {
+	// Fake for use by newsletter.js
+	Module = {};
+	Module.definitions = {};
+	Module.register = function (name, moduleDefinition) {
+		Module.definitions[name] = moduleDefinition;
+	};
+
+	before(function () {
+		// load newsfeed.js
+		require("../mmm-testing.js");
+		//Module.definitions.mmm-testing.config = {};
+	});
+});
