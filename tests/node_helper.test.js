8697a3ff18c2c4cf66526f283a2b640efb8d3d9c
Ian <ianperrin@gmail.com>
2021-03-13 16:30:40 +0000

    Update node_helper.test.js

--- tests/node_helper.test.js
+++ tests/node_helper.test.js
@@ -4,14 +4,17 @@ const moduleAlias = require('module-alias');
 describe("Functions into node_helper.js", function () {
 	before(function () {
 		console.log("Before function");
-		moduleAlias.addAlias('node_helper', "../../js/node_helper.js")
-		const ModuleNodeHelper = require("../node_helper.js");
+		moduleAlias.addAlias('node_helper', "../../js/node_helper.js");
+		var Module = require("../node_helper.js");
+		var m = new Module();
+		m.setName("mmm-testing");
+		m.setPath(path.resolve("../"));
 	});
 
 	describe("callApi", function () {
 		console.log("callApi tests...");
 		it(`for 1 should be return 1}`, function () {
-			expect(ModuleNodeHelper.callApi()).to.equal(1);
+			expect(m.callApi()).to.equal(1);
 		});
 	});
 });
