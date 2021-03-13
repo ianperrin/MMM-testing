9df4bec69f3d46571d8edbbf952ed89fb740bbec
Ian <ianperrin@gmail.com>
2021-03-13 23:06:18 +0000

    Update mmm-testing.test.js

--- tests/mmm-testing.test.js
+++ tests/mmm-testing.test.js
@@ -1,22 +1,20 @@
 const expect = require("chai").expect;
 
 describe("Functions into mmm-testing.js", function () {
-	before(function () {
-		console.log("Before function");
-		Module = {};
-		config = {};
-		Module.definitions = {};
-		Module.register = function (name, moduleDefinition) {
-			Module.definitions[name] = moduleDefinition;
-		};
-		require("../mmm-testing.js");
-		Module.definitions["mmm-testing"].config = {};
-	});
+  before(function () {
+    Module = {};
+    config = {};
+    Module.definitions = {};
+    Module.register = function (name, moduleDefinition) {
+      Module.definitions[name] = moduleDefinition;
+    };
+    require("../mmm-testing.js");
+    Module.definitions["mmm-testing"].config = {};
+  });
 
-	describe("roundValue", function () {
-		console.log("Here");
-		it(`for 4 should be return 1}`, function () {
-			expect(Module.definitions["mmm-testing"].roundValue(4)).to.equal(1);
-		});
-	});
+  describe("roundValue", function () {
+    it(`for 4 should be return 1}`, function () {
+      expect(Module.definitions["mmm-testing"].roundValue(4)).to.equal(1);
+    });
+  });
 });
